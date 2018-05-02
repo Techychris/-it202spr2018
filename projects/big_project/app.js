6581ee9ec301122b0fd31097a814a25642a005e1
Chris Vinzons <chrisv@Drax>
2018-05-02 12:01:51 -0500

    Service worker and manifest

--- projects/big_project/app.js
+++ projects/big_project/app.js
@@ -1,2 +1,14 @@
 import {MDCRipple} from '@material/ripple';
 const ripple = new MDCRipple(document.querySelector('.foo-button'));
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
